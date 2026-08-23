b4f957ebe4bc84188cc945d4dccebef28fb5db3c
yuriykotlov <ykcontactreal@gmail.com>
2026-08-23 21:30:49 +0100

    clean up and implement ShellExecuteW

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 #include <string>
+#include <format>
 
 #include <windows.h>
 #include <shellapi.h>
 #include <conio.h>
 
 #define STEAM_PREFIX "steam://openurl/"
+
+#ifndef ENABLE_VIRTUAL_TERMINAL_PROCESSING
+    #define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
+#endif
+
+void enable_ansi_codes() {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hConsole == INVALID_HANDLE_VALUE) return;
+
+    DWORD dwMode = 0;
+    if (!GetConsoleMode(hConsole, &dwMode)) return;
+
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    SetConsoleMode(hConsole, dwMode);
+}
 
 std::string get_clipboard_text(){
     if(!OpenClipboard(nullptr)){
@@ -58,8 +74,8 @@ std::string get_line(const std::string &copiedText){
     return text;
 }
 
-void run_command(const std::string &target){
-    HINSTANCE result = ShellExecuteA(nullptr, "open", target.c_str(), nullptr, nullptr, SW_SHOWNORMAL);
+void run_command(const std::wstring &target){
+    HINSTANCE result = ShellExecuteW(nullptr, L"open", target.c_str(), nullptr, nullptr, SW_SHOWNORMAL);
 
     // mostly likely will never throw error since STEAM_PREFIX is valid
     if(reinterpret_cast<INT_PTR>(result) <= 32){
@@ -67,16 +83,28 @@ void run_command(const std::string &target){
     }
 
     std::cout << "\nif steam doesn't open anything, your url was invalid or steam was closed.\n";
-    std::cout << "[ press any button to close ]";
-    _getch();
 }
 
 int main(){
-    std::string target{STEAM_PREFIX + get_line(get_clipboard_text())};
+    enable_ansi_codes();
 
-    std::cout << "directing to \"" + target + "\" ...\n";
+    char option{};
+    
+    while(std::tolower(option) != 'q'){
+        std::cout << "\033[H\033[2J" << std::flush;
 
-    run_command(target);
+        std::string target{STEAM_PREFIX + get_line(get_clipboard_text())};
+
+        std::cout << "directing to \"" + target + "\" ...\n";
+
+        // convert string to wstring
+        run_command(
+            {target.begin(), target.end()}
+        );
+        
+        std::cout << "[ press Q to close or ENTER for a new url ]\n\n";
+        option = std::cin.get();
+    }
 
     return 0;
 }
