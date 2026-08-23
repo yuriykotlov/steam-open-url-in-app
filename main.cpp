e25e008b31d13b61a0710f62121de1c7169352fc
yuriykotlov <ykcontactreal@gmail.com>
2026-08-23 18:44:52 +0100

    reading copied text on clipboard working now

--- main.cpp
+++ main.cpp
@@ -5,6 +5,37 @@
 
 #define STEAM_PREFIX "steam://openurl/"
 
+std::string get_clipboard_text(){
+    if(!OpenClipboard(nullptr)){
+        CloseClipboard();
+        return {};
+    };
+
+    HANDLE data = GetClipboardData(CF_TEXT);
+    if(data == nullptr){
+        CloseClipboard();
+        return {};
+    }
+
+    const char *pszText = static_cast<char*>(GlobalLock(data));
+    if(pszText == nullptr){
+        CloseClipboard();
+        return {};
+    }
+
+    // [personal note]
+    // originally tried to use string_view, but it is a pointer to the string
+    // supplied, so after unlocking the data/closing the clipboard the pointer
+    // is not guaranteed to be the same value or exist at all, so we need to
+    // copy the value instead
+    std::string copiedText{pszText};
+
+    GlobalUnlock(data);
+    CloseClipboard();
+    
+    return copiedText;
+}
+
 int main(){
     // std::string target{};
 
@@ -13,9 +44,20 @@ int main(){
 
     //ShellExecuteA(nullptr, "open", (STEAM_PREFIX + target).c_str(), nullptr, nullptr, SW_SHOWNORMAL);
 
-    auto result = ShellExecuteA(nullptr, "open", "powershell -command \"Get-Clipboard\"", nullptr, nullptr, SW_SHOWNORMAL);
+    // auto result = ShellExecuteA(
+    //     nullptr,
+    //     "open",
+    //     "powershell",
+    //     "-command \"Get-Clipboard\"",
+    //     nullptr,
+    //     SW_SHOWNORMAL
+    // );
 
-    std::cout << result;
+    // std::cout << (INT_PTR)result;
+
+    std::string copiedText{get_clipboard_text()};
+
+    std::cout << copiedText;
 
     return 0;
 }
